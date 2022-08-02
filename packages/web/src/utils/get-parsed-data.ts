a1df4aa58730f4a2f33aa4b710192850ec6db4f9
Vladislav <vladislav.savosko@smile2impress.com>
2022-08-03 01:26:20 +0400

    [CP-10] Implementing a lib for the web and splitting the project into separate packages

---
+++ packages/web/src/utils/get-parsed-data.ts
@@ -0,0 +1,27 @@
+import { ValueWithRegExpType, IGetParsedData } from './get-parsed-data.types';
+
+export const getParsedData = ({ sourceFileData, userFileData }: IGetParsedData) => {
+  const result: Record<string, string> = {};
+
+  userFileData.forEach(({ columnName, newColumnName, value }) => {
+    const sourceFileColumnData = sourceFileData[columnName];
+
+    if (!sourceFileColumnData || !value) return;
+
+    const columnNameInPreparedFile = newColumnName || columnName;
+
+    if (value instanceof Object) {
+      const { regExp, captureGroup } = value as ValueWithRegExpType;
+
+      return (result[columnNameInPreparedFile] = decodeURIComponent(
+        sourceFileColumnData.match(regExp)?.[captureGroup || 0] || ''
+      ));
+    }
+
+    result[columnNameInPreparedFile] = decodeURIComponent(
+      sourceFileColumnData.match(value as string)?.[0] || ''
+    );
+  });
+
+  return result;
+};
